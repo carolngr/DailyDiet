1cd3b7950f7a079dd7730d4c409bd46cf807dfcb
Caroline Nogueira <carol.ngr8@gmail.com>
2024-07-07 21:06:01 -0300

    :sparkles: feat: initial commit

---
+++ App.tsx
@@ -0,0 +1,22 @@
+import { ActivityIndicator } from "react-native";
+
+import { ThemeProvider } from "styled-components";
+import {
+  useFonts,
+  NunitoSans_400Regular,
+  NunitoSans_700Bold,
+} from "@expo-google-fonts/nunito-sans";
+
+import Home from "@screens/Home";
+import theme from "@theme/index";
+import { Loading } from "@components/Loading";
+
+export default function App() {
+  const [fontsLoaded] = useFonts({ NunitoSans_400Regular, NunitoSans_700Bold });
+
+  return (
+    <ThemeProvider theme={theme}>
+      {!fontsLoaded ? <Home /> : <Loading />}
+    </ThemeProvider>
+  );
+}
