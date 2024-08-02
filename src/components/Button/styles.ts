956dd5ce3c88d6f793dada07c0bb275b3214e600
Caroline Nogueira <carol.ngr8@gmail.com>
2024-08-02 18:57:14 -0300

    :lipstick: feat: style button

--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -4,14 +4,17 @@ import { Plus } from "phosphor-react-native";
 export const Container = styled.TouchableOpacity`
   background-color: ${({ theme }) => theme.COLORS.GRAY_2};
   border-radius: 6px;
+  flex-direction: row;
+  align-items: center;
+  padding: 16px 24px;
+  justify-content: center;
+  gap: 10px;
 `;
 
 export const Title = styled.Text`
   color: #fff;
   font-size: ${({ theme }) => theme.COLORS.WHITE};
   font-weight: bold;
-  text-align: center;
-  padding: 16px 24px;
 `;
 
 export const IconButton = styled(Plus).attrs(({ theme }) => ({
